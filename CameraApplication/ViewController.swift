95c9d5b9fdc18c36200fb77d624499626f7aabb7
Renma Hirano <everydaysai@gmail.com>
2021-06-29 11:13:54 +0900

    Initial Commit

---
+++ CameraApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApplication
+//
+//  Created by Renma Hirano on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
